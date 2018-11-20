d8225935708dabf83a2dd293e46306a6549a8747
28810 <28810@YEXIANGQIN>
2018-11-20 19:49:41 +0800

    v1.2.1

--- Npgsql/ExecuterUtils.cs
+++ Npgsql/ExecuterUtils.cs
@@ -41,7 +41,7 @@ namespace Npgsql {
 		}
 
 		private static DateTime dt1970 = new DateTime(1970, 1, 1);
-		private static Random rnd = new Random();
+		private static ThreadLocal<Random> rnd = new ThreadLocal<Random>();
 		private static readonly int __staticMachine = ((0x00ffffff & Environment.MachineName.GetHashCode()) +
 #if NETSTANDARD1_5 || NETSTANDARD1_6
 			1
@@ -50,7 +50,7 @@ namespace Npgsql {
 #endif
 			) & 0x00ffffff;
 		private static readonly int __staticPid = Process.GetCurrentProcess().Id;
-		private static int __staticIncrement = rnd.Next();
+		private static int __staticIncrement = rnd.Value.Next();
 		/// <summary>
 		/// 生成类似Mongodb的ObjectId有序、不重复Guid
 		/// </summary>
@@ -59,7 +59,7 @@ namespace Npgsql {
 			var now = DateTime.Now;
 			var uninxtime = (int) now.Subtract(dt1970).TotalSeconds;
 			int increment = Interlocked.Increment(ref __staticIncrement) & 0x00ffffff;
-			var rand = rnd.Next(0, int.MaxValue);
+			var rand = rnd.Value.Next(0, int.MaxValue);
 			var guid = $"{uninxtime.ToString("x8").PadLeft(8, '0')}{__staticMachine.ToString("x8").PadLeft(8, '0').Substring(2, 6)}{__staticPid.ToString("x8").PadLeft(8, '0').Substring(6, 2)}{increment.ToString("x8").PadLeft(8, '0')}{rand.ToString("x8").PadLeft(8, '0')}";
 			return Guid.Parse(guid);
 		}
